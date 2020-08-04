5948a46d772da92e40099b775c80a0b2476c65f6
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2020-08-04 08:54:32 +0200

    Use single quote for strings

--- ublox-agps.py
+++ ublox-agps.py
@@ -19,31 +19,31 @@ if not os.access(args.device, os.W_OK):
     print(f'Error: device {args.device} is not writable', file=sys.stderr)
     sys.exit(1)
 
-url = f"http://online-live1.services.u-blox.com/GetOnlineData.ashx?token={args.token};gnss=gps;datatype=eph,alm,aux,pos;filteronpos;format=aid"
-print("Downloading A-GPS data")
+url = f'http://online-live1.services.u-blox.com/GetOnlineData.ashx?token={args.token};gnss=gps;datatype=eph,alm,aux,pos;filteronpos;format=aid'
+print('Downloading A-GPS data')
 r = requests.get(url)
 
 ser = serial.Serial(args.device, 9600)
-print("Waiting for GPS to be free")
+print('Waiting for GPS to be free')
 drainer = True
 while drainer:
     drainer = ser.inWaiting()
     ser.read(drainer)
 
-print(f"Writing AGPS data to {args.device}")
+print(f'Writing AGPS data to {args.device}')
 ser.write(r.content)
-print("Done")
+print('Done')
 
-print("Reading GPS data: hit CTRL-C to quit")
+print('Reading GPS data: hit CTRL-C to quit')
 buffer = True
-message = b""
+message = b''
 try:
     while buffer:
         buffer = ser.read()
-        if buffer == b"$":
-            if message.startswith(b"$GPGGA"):
+        if buffer == b'$':
+            if message.startswith(b'$GPGGA'):
                 print(message.strip().decode())
-            message = b""
+            message = b''
         message = message + buffer
 except KeyboardInterrupt:
     ser.close()
