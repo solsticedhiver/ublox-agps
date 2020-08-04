df716cb635c39a2f82bae58510ff378c07d659e1
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2020-08-04 11:23:39 +0200

    Check the binary data before sending it to the GPS device

--- ublox-agps.py
+++ ublox-agps.py
@@ -5,6 +5,21 @@ import serial
 import sys
 import argparse
 import os
+import struct
+
+DEBUG = False
+
+def checksum(data):
+    a = b = 0
+    for d in data:
+        a += d
+        a = a & 0xFF
+        b += a
+        b = b & 0xFF
+    return (a,b)
+
+def is_checksum_ok(ck_a, ck_b, data):
+    return checksum(data) == (ck_a, ck_b)
 
 parser = argparse.ArgumentParser(description='Retrieve aiding data and update GPS module')
 parser.add_argument('-t', '--token', required=True, help='your token to access AssistNow data site')
@@ -24,6 +39,27 @@ print('Downloading A-GPS data')
 r = requests.get(url)
 if r.status_code != 200:
     print(f'Error {r.status_code} {r.content.decode()}', file=sys.stderr)
+    sys.exit(1)
+data = r.content
+
+# check the checksum of the downloaded data
+is_valid = data[0] == 0xb5 and data[1] == 0x62 # first header
+indx = 0
+while is_valid and indx < len(data):
+    if data[indx] == 0xb5 and data[indx+1] == 0x62:
+        length = struct.unpack('<H', bytes(data[indx+4:indx+6]))[0]
+        payload = data[indx+2:indx+5+length+1]
+        check = is_checksum_ok(data[indx+5+length+1], data[indx+5+length+2], payload)
+        if DEBUG:
+            ok = "OK" if check else "NOT OK"
+            print(f'Found header {hex(data[indx+2])}/{hex(data[indx+3])} at {indx}, {length} bytes, checksum {ok}')
+        indx += 5+length+3
+        is_valid = is_valid and check
+
+if is_valid:
+    print('Checksums are OK')
+else:
+    print('Error: checksums are NOT OK', file=sys.stderr)
     sys.exit(1)
 
 ser = serial.Serial(args.device, 9600)
